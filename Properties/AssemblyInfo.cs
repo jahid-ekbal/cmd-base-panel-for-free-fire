95e97110a163997fc29701a03d42664fed6fc40a
Jahid Ekbal Mallick <jahidekbalmallick@gmail.com>
2026-05-05 01:47:17 +0530

    Restructure project files and add VS metadata
    Move project files from Free-Fire-Aimbot-Ingame-main to repository root and rename key files (AimbotTest.csproj, AimbotTest.sln, App.config). Rename GTCMemory.cs to JahidMemory.cs and delete the Free-Fire-Aimbot-Ingame README. Add Visual Studio workspace metadata (.vs/*), solution DB (.vs/slnx.sqlite) and build artifacts (obj/*). Perform minor whitespace/format cleanups in project and config files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AimbotTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FS29894")]
+[assembly: AssemblyProduct("AimbotTest")]
+[assembly: AssemblyCopyright("Copyright © FS29894 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea92259f-462a-47ba-a7f0-4e7d0d3dcc92")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
